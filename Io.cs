bbf47eb2c104f076291aa5e01e172fca84b39b26
luhan <omarlujan.h@gmail.com>
2025-10-20 22:07:02 -0500

    add: static class

---
+++ Io.cs
@@ -0,0 +1,22 @@
+﻿namespace Luhan.IO;
+
+public static class Io
+{
+    public static void Printf(object? value)
+    {
+        Console.WriteLine(value);
+    }
+
+    public static void Printf(params object?[] values)
+    {
+        Console.WriteLine(string.Join(" ", values));
+    }
+
+    public static void Log(object? value)
+    {
+        var originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {value}");
+        Console.ForegroundColor = originalColor;
+    }
+}
